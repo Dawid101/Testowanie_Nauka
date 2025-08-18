f341f7894af1ca2b0b89da8eab79bab67e82c52a
dawid101 <dawcik35@gmail.com>
2025-08-18 21:01:13 +0200

    task life cycle

---
+++ src/test/java/pl/coderslab/cycle/TaskManagerTest.java
@@ -0,0 +1,53 @@
+package pl.coderslab.cycle;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskManagerTest {
+
+    private TaskManager taskManager;
+
+    @BeforeEach
+    void setTaskManager() {
+        taskManager = new TaskManager();
+    }
+
+    @Test
+    void testAddTask() {
+        Task task = new Task(1, "Cleaning", "Clean my room", "To do");
+        taskManager.addTask(task);
+        assertTrue(taskManager.getTasks().contains(task));
+    }
+
+    @Test
+    void testRemoveExistingTaskById() {
+        Task task = new Task(1, "Cleaning", "Clean my room", "To do");
+        taskManager.addTask(task);
+        assertTrue(taskManager.removeTaskById(1));
+        assertFalse(taskManager.getTasks().contains(task));
+    }
+
+    @Test
+    void testRemoveNonExistingTaskById() {
+        assertFalse(taskManager.removeTaskById(2));
+    }
+
+    @Test
+    void testChangeExistingTaskStatus() {
+        Task task = new Task(1, "Cleaning", "Clean my room", "To do");
+        taskManager.addTask(task);
+        taskManager.changeTaskStatus(1, "In progress");
+        assertEquals("In progress", task.getStatus());
+    }
+
+    @Test
+    void testChangeNonExistingTaskStatus() {
+        Task task = new Task(2, "Cleaning", "Clean my room", "To do");
+        taskManager.addTask(task);
+        taskManager.changeTaskStatus(1, "In progress");
+        int size = taskManager.getTasks().size();
+        assertTrue(size > 0);
+    }
+}
